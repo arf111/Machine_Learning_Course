d3ed5e542400da7c5ec9a60cd79d5bdb296d5085
arefeen <krsultan069@gmail.com>
2022-10-24 21:27:10 -0600

    updated sh file.

--- Ensemble Learning/run.sh
+++ Ensemble Learning/run.sh
@@ -2,3 +2,9 @@
 python3 run_adaboost.py
 python3 run_bagged_tree.py
 python3 run_random_forest.py
+python3 run_random_forest2.py
+python3 run_random_forest3.py
+python3 run_random_forest4.py
+python3 run_bagged_tree2.py
+python3 credit_dataset_bagging.py
+python3 credit_dataset_random_forest.py
